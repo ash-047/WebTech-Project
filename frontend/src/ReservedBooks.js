c9387b51f53b89d0327577bd4c69fa92dfb4ecf6
ash-047 <ashfaq.am220@gmail.com>
2023-11-23 15:01:29 +0530

    test commit

--- frontend/src/ReservedBooks.js
+++ frontend/src/ReservedBooks.js
@@ -9,7 +9,8 @@ const ReservedBooksPage = () => {
     <div className='ever'>
       <section className="main-content">
         <div className="reservedbooks-results">
-          <h2 align="center">Reserve Books</h2>
+          <h2 style={{ textAlign: 'center', fontSize: '36px', fontFamily: 'Arial, sans-serif', color: '#236b92', animation: 'slideIn 0.5s ease-in-out' }}>Reserve Books</h2>
+          <hr width="50" color="#000000" className="hr_lines"/>
           <ul className="results-list" id="books-results">
             <li>
               <div className="result-item">
